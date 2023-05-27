51f8aaa0158f694b98924410483e5f3b50c4daa4
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-05-28 00:33:16 +0530

    inserstion

--- InsertionBinarySearchKey.js
+++ InsertionBinarySearchKey.js
@@ -16,7 +16,42 @@ class InsertionBinary{
         if(this.root === null ){
             this.root = newnode
             return this;
+        }else{
+            var current = this.root
+            while(true){
+               if (value < current.value) {
+                if(current.left === null){
+                    current.left = newnode
+                    return this
+                }else{
+                    current = current.left
+                }
+                
+               } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newnode
+                        return this
+                        
+                    } else {
+                        current = current.right
+                        
+                    }
+                
+               }
+
+            }
         }
     }
 }
 
+
+var tree = new InsertionBinary() 
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
+
+console.log(tree);

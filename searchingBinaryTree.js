93be8e9c179a575f5cd46d10f5b7aa9e145d095f
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-05-28 18:19:53 +0530

    insertion and search

--- searchingBinaryTree.js
+++ searchingBinaryTree.js
@@ -11,15 +11,35 @@ class BinarySearch{
         this.root = null
     }
     isEmpty(){
-        return this.root = null
+        return this.root === null
     }
 
     insert(value){
         const newNode = new Node(value)
         if(this.isEmpty()){
             this.root = newNode
+        }else{
+            this.insertNode(this.root,newNode)
+
         }
-        
+
+
+    }
+
+    insertNode(root,newNode){
+        if(newNode.value < root.value){
+            if(root.left === null ){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left,newNode)
+            }
+        }else{
+             if(root.right === null){
+                root.right = newNode
+             }else{
+                this.insertNode(root.right,newNode)
+             }
+        }
 
     }
 
@@ -41,3 +61,15 @@ class BinarySearch{
     }
 
 } 
+
+const bst =  new BinarySearch()
+
+ bst.insert(10)
+ bst.insert(5)
+ bst.insert(15)
+ 
+console.log(bst.root);
+
+console.log(bst.search(bst.root,10));
+console.log(bst.search(bst.root,5));
+console.log(bst.search(bst.root,15));
